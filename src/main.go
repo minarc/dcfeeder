f404b8a8e551d840463a3d6df6dfee2c799f9ae2
mowinckel <ragnhildmowinckel@gmail.com>
2020-03-31 01:46:27 +0900

    test

--- src/main.go
+++ src/main.go
@@ -275,8 +275,8 @@ func main() {
 
 	client = redis.NewClient(&redis.Options{
 		// Addr: "seoul.arfrumo.codes:6379",
-		Addr: "34.64.196.220:6379",
-		// Addr: "127.0.0.1:6379",
+		// Addr: "34.64.196.220:6379",
+		Addr:     "127.0.0.1:6379",
 		Password: "WCkaZYzyhYR62p42VddCJba7Kn14vdvw",
 		DB:       0,
 	})
